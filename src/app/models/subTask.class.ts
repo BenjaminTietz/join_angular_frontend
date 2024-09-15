c9b944a856bda57f78672e66e4dfe91a175a267f
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-15 11:08:34 +0200

    feat(subtask): Add SubTask class to support task breakdown
    - Implemented SubTask class to allow tasks to be broken down into smaller, manageable units. This class is referenced within Task for more detailed task management.

---
+++ src/app/models/subTask.class.ts
@@ -0,0 +1,19 @@
+import { Task } from './task.class';
+
+export class SubTask {
+  taskId: Task['taskId'];
+  subTaskId: string;
+  title: string;
+  checked: boolean;
+  createdAt: string;
+  createdBy: string;
+
+  constructor(obj?: any) {
+    this.taskId = obj ? obj.id : '';
+    this.subTaskId = obj ? obj.subTaskId : '';
+    this.title = obj ? obj.title : '';
+    this.checked = obj ? obj.checked : false;
+    this.createdAt = obj ? obj.createdAt : '';
+    this.createdBy = obj ? obj.createdBy : '';
+  }
+}
