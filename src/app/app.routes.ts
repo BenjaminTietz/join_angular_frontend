fe55aef6fdb2ca6b0722eb24ee6ae98532350ccb
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-15 12:40:46 +0200

    feat(routes): Configure and extend routing for pre-login and post-login components
    - Set up routing for LandingComponent with child routes for LoginComponent and SignupComponent under the pre-login path.
    - Extended routing for HomescreenComponent with child routes for SummaryComponent, AddTaskComponent, BoardComponent, ContactsComponent, ImprintComponent, and PrivacyPolicyComponent under the post-login path.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,38 @@
 import { Routes } from '@angular/router';
 import { HomescreenComponent } from './post-login/homescreen/homescreen.component';
+import { LandingComponent } from './pre-login/landing/landing.component';
+import { LoginComponent } from './pre-login/login/login.component';
+import { SignupComponent } from './pre-login/signup/signup.component';
+import { SummaryComponent } from './post-login/summary/summary.component';
+import { AddTaskComponent } from './post-login/add-task/add-task.component';
+import { BoardComponent } from './post-login/board/board.component';
+import { ContactsComponent } from './post-login/contacts/contacts.component';
+import { ImprintComponent } from './legal/imprint/imprint.component';
+import { PrivacyPolicyComponent } from './legal/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   // pre-login component routes
+  {
+    path: '',
+    component: LandingComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+    ],
+  },
 
   // post-login component routes
 
   {
     path: 'home',
     component: HomescreenComponent,
+    children: [
+      { path: 'summary', component: SummaryComponent },
+      { path: 'add-task', component: AddTaskComponent },
+      { path: 'board', component: BoardComponent },
+      { path: 'contacts', component: ContactsComponent },
+      { path: 'imprint', component: ImprintComponent },
+      { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    ],
   },
 ];
