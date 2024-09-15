e3750fa38ab38d52bc941ff558394c3b2eeaa4bf
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-15 12:37:16 +0200

    feat(pre-login): Integrate header, router-outlet, and footer into landing component
    - Updated Landing component in the pre-login folder to include HeaderApp, router-outlet, and FooterApp for a complete layout structure.

--- src/app/pre-login/landing/landing.component.ts
+++ src/app/pre-login/landing/landing.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../../post-login/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, RouterOutlet],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.component.scss',
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}
