4ba3588b0d11a46f539a4e1804dd2caf926a6ea6
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-17 11:03:07 +0200

    feat(pre-login / signup): implement reactive form for signup component
    - Added reactive form setup in SignupComponent
    - Created FormGroup with controls for name, email, password, confirmPassword, and privacy
    - Implemented password length validation and password confirmation check
    - Updated HTML template to use form controls
    - Modified submit button to be enabled only when the form is valid

--- src/app/pre-login/signup/signup.component.ts
+++ src/app/pre-login/signup/signup.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  signupForm!: FormGroup;
 
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.createSignupForm();
+  }
+
+  createSignupForm() {
+    this.signupForm = this.fb.group(
+      {
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+        privacy: [false, Validators.requiredTrue],
+      },
+      {
+        validator: this.passwordMatchValidator,
+      }
+    );
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+      // connect with auth service to communicate with backend
+    } else {
+      console.log('Invalid Form');
+    }
+  }
 }
