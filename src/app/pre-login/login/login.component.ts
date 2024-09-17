f0472a5cc1b3bee73e3c68baa23e9c85c1c0cc12
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-17 10:51:08 +0200

    feat(pre-login/login): implement reactive form for login component
    - Added reactive form setup in LoginComponent
      - Created FormGroup with controls for email, password, and remember me checkbox
      - Implemented validation for email and password fields
    - Updated HTML template to use form controls
    - Modified submit button to be enabled only when the form is valid

--- src/app/pre-login/login/login.component.ts
+++ src/app/pre-login/login/login.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-
+import {
+  FormGroup,
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      remember: [false],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      console.log('Invalid Form');
+    }
+  }
 }
