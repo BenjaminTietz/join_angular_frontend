3ec1b7d586ccfc3f66aa17ec9790ca307d345ad7
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-18 15:10:22 +0200

    refactor(post-login / add-task comp): restructure form to reactive form and update TypeScript logic
    - Replaced the template-driven form with a reactive form using FormBuilder.
    - Updated form controls for taskTitle, taskDescription, taskDueDate, taskPriority, taskAssignedTo, category, and subtask.
    - Dynamically populated 'Assigned to' and 'Category' dropdowns with observable data and predefined categories.
    - Added form validation and bound the submit button to the form’s validity state.
    - Introduced methods to handle form submission and clearing of form data.
    - Used the 'ReactiveFormsModule' in the component module imports for reactive form handling.

--- src/app/post-login/add-task/add-task.component.ts
+++ src/app/post-login/add-task/add-task.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Contact } from '../../models/contact.class';
+import { DatabaseService } from '../../services/database.service';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.scss'
+  styleUrl: './add-task.component.scss',
 })
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit {
+  addTaskForm!: FormGroup;
+  categories = [
+    { value: 'marketing', label: 'Marketing', class: 'marketing' },
+    { value: 'sales', label: 'Sales', class: 'sales' },
+    { value: 'development', label: 'Development', class: 'development' },
+    { value: 'accounting', label: 'Accounting', class: 'accounting' },
+    { value: 'purchase', label: 'Purchase', class: 'purchase' },
+  ];
+  contacts$!: Observable<Contact[]>;
 
+  constructor(
+    private fb: FormBuilder,
+    private databaseService: DatabaseService
+  ) {
+    this.addTaskForm = this.fb.group({
+      taskTitle: ['', Validators.required],
+      taskDescription: [''],
+      taskDueDate: ['', Validators.required],
+      taskPriority: ['', Validators.required],
+      taskAssignedTo: [''],
+      category: ['', Validators.required],
+      subtask: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    this.contacts$ = this.databaseService.getContacts();
+  }
+
+  onSubmit() {
+    if (this.addTaskForm.valid) {
+      console.log(this.addTaskForm.value);
+    }
+  }
+  onClear() {
+    this.addTaskForm.reset();
+  }
 }
