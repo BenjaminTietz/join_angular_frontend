fb90902b41b107d0e7314badd3749e71c3ef545a
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-18 11:41:50 +0200

    feat(post login-contact comp): Integrate dynamic contact data
    - Updated the ContactsComponent to dynamically display contact data from the backend.
      - Replaced hardcoded contact information with dynamic data using the contacts observable.
      - Bound the `contacts$` observable in `ngOnInit` to fetch the list of contacts from the DatabaseService.
      - Modified the HTML template to loop through the contacts and display each contact's name and email dynamically.
    
    - Adjusted HTML to use Angular's async pipe for cleaner subscription handling.

--- src/app/post-login/contacts/contacts.component.ts
+++ src/app/post-login/contacts/contacts.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Contact } from '../../models/contact.class';
+import { DatabaseService } from '../../services/database.service';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrl: './contacts.component.scss',
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
+  contacts$!: Observable<Contact[]>;
+  constructor(private databaseService: DatabaseService) {}
 
+  ngOnInit(): void {
+    this.contacts$ = this.databaseService.getContacts();
+  }
 }
