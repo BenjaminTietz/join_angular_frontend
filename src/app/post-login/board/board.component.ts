29cef7b3314358a48aa8dd5fe614407114134e2d
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-18 11:43:56 +0200

    feat(post-login, board comp): Display dynamic task data and priority icons in BoardComponent
    - Integrated dynamic task data from the backend into the BoardComponent.
      - Bound the `tasks$` observable in `ngOnInit` to load tasks from the DatabaseService.
      - Replaced hardcoded task details (title, description, category) with dynamic content using Angular's async pipe.
    
    - Implemented dynamic priority icon display:
      - Added a `priorityIcons` map to associate task priority (HIGH, MEDIUM, LOW) with their respective icons.
      - Created `getPriorityIcon` method to return the correct icon based on task priority.
      - Updated HTML template to use `getPriorityIcon` for dynamic priority icon rendering.
    
    - Modified HTML structure to handle task rendering within the TODO column dynamically.

--- src/app/post-login/board/board.component.ts
+++ src/app/post-login/board/board.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../../services/database.service';
+import { Task } from '../../models/task.class';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
+  tasks$!: Observable<Task[]>;
+  priorityIcons: { [key in 'HIGH' | 'MEDIUM' | 'LOW']: string } = {
+    HIGH: '/assets/img/icons/task_prio_urgent.png',
+    MEDIUM: '/assets/img/icons/task_prio_medium.png',
+    LOW: '/assets/img/icons/task_prio_low.png',
+  };
+  constructor(private databaseService: DatabaseService) {}
 
+  ngOnInit(): void {
+    this.tasks$ = this.databaseService.getTasks();
+  }
+
+  getPriorityIcon(priority: string): string {
+    return (
+      this.priorityIcons[priority as 'HIGH' | 'MEDIUM' | 'LOW'] ||
+      '/assets/img/icons/task_prio_default.png'
+    );
+  }
 }
