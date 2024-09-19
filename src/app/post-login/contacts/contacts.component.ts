71ef61b4c2758294b2086293fd2ca4ca543dfe94
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-19 10:12:24 +0200

    feat(post-login / contacts comp): implement reactive form and methods in Contacts component
    - Declared reactive form `addContactForm` with form controls for 'name', 'email', and 'phone'
    - Added `onSubmit` method to handle form validation and submission via DatabaseService
    - Implemented logic for creating a new contact and handling success, error, and completion states
    - Added `onClear` method to reset the form

--- src/app/post-login/contacts/contacts.component.ts
+++ src/app/post-login/contacts/contacts.component.ts
@@ -3,18 +3,34 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Contact } from '../../models/contact.class';
 import { DatabaseService } from '../../services/database.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss',
 })
 export class ContactsComponent implements OnInit {
   contacts$!: Observable<Contact[]>;
   contactDetail: any | null = null;
-  constructor(private databaseService: DatabaseService) {}
+  addContactForm!: FormGroup;
+  constructor(
+    private databaseService: DatabaseService,
+    private fb: FormBuilder
+  ) {
+    this.addContactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
     this.contacts$ = this.databaseService.getContacts();
@@ -26,5 +42,34 @@ export class ContactsComponent implements OnInit {
       this.contactDetail = contact;
     });
     console.log(this.contactDetail);
+  }
+
+  onSubmit() {
+    if (this.addContactForm.valid) {
+      const newContact: Contact = {
+        id: '',
+        name: this.addContactForm.value.name,
+        email: this.addContactForm.value.email,
+        phone: this.addContactForm.value.phone,
+        createdAt: '',
+        createdBy: '',
+      };
+      console.log('Creating contact:', newContact);
+      this.databaseService.createContact(newContact).subscribe({
+        next: (contact) => {
+          console.log('Contact created:', contact);
+          // todo show success message / reset form
+        },
+        error: (error) => {
+          console.error('Error creating contact:', error);
+        },
+        complete: () => {
+          console.log('Contact creation process completed.');
+        },
+      });
+    }
+  }
+  onClear() {
+    this.addContactForm.reset();
   }
 }
