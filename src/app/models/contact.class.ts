ac9fc9d13694fd1c604e8dc5624c07beb929c4c7
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-20 14:34:39 +0200

    refactor: Extend Contact model and serializer
    - Enhanced frontend Contact class to support `initials` and `color` attributes.

--- src/app/models/contact.class.ts
+++ src/app/models/contact.class.ts
@@ -4,6 +4,8 @@ export class Contact {
   name: string;
   email: string;
   phone: string;
+  initials: string;
+  color: string;
   createdAt: string;
   createdBy: User['userId'];
 
@@ -12,6 +14,8 @@ export class Contact {
     this.name = obj ? obj.name : '';
     this.email = obj ? obj.email : '';
     this.phone = obj ? obj.phone : '';
+    this.initials = obj ? obj.initials : '';
+    this.color = obj ? obj.color : '';
     this.createdAt = obj ? obj.createdAt : '';
     this.createdBy = obj ? obj.createdBy : '';
   }
