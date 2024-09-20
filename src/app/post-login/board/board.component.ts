9c9f485b91456cafd8ca4f3fc9e87eae53ba53da
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-20 23:34:26 +0200

    feat(post-login / board): implement drag-and-drop feature for task board
    - Updated HTML structure to support drag-and-drop functionality.
    - Enhanced TypeScript logic to handle drag events and update task status.
    - Adjusted CSS styling for better visual feedback during drag-and-drop actions.
    - Integrated task status update via DatabaseService on drop.
    - Added logging for debugging purposes to track task states during drag events.

--- src/app/post-login/board/board.component.ts
+++ src/app/post-login/board/board.component.ts
@@ -21,11 +21,34 @@ export class BoardComponent implements OnInit {
   };
   showAddTaskOverlay = false;
   selectedStaus = '';
-  constructor(private databaseService: DatabaseService) {}
+  todoTasks: Task[] = [];
+  inProgressTasks: Task[] = [];
+  awaitFeedbackTasks: Task[] = [];
+  doneTasks: Task[] = [];
+  daggedTaskId = '';
 
   ngOnInit(): void {
     this.tasks$ = this.databaseService.getTasks();
+    this.tasks$.subscribe((tasks) => {
+      console.log('Tasks received:', tasks);
+
+      this.todoTasks = tasks.filter((task) => task.status === 'todo');
+      this.inProgressTasks = tasks.filter(
+        (task) => task.status === 'inProgress'
+      );
+      this.awaitFeedbackTasks = tasks.filter(
+        (task) => task.status === 'awaitFeedback'
+      );
+      this.doneTasks = tasks.filter((task) => task.status === 'done');
+
+      console.log('Todo Tasks:', this.todoTasks);
+      console.log('In Progress Tasks:', this.inProgressTasks);
+      console.log('Await Feedback Tasks:', this.awaitFeedbackTasks);
+      console.log('Done Tasks:', this.doneTasks);
+    });
   }
+
+  constructor(private databaseService: DatabaseService) {}
 
   getPriorityIcon(priority: string): string {
     return (
@@ -40,5 +63,33 @@ export class BoardComponent implements OnInit {
   }
   handleCloseAddTaskOverlay() {
     this.showAddTaskOverlay = false;
+  }
+
+  //drag and drop
+
+  onDrag(event: DragEvent, index: number, taskId: string, status: string) {
+    console.log('onDrag', index);
+    console.log('taskId', taskId);
+    console.log('status', status);
+    this.daggedTaskId = taskId;
+  }
+
+  onDrop(event: DragEvent, status: string) {
+    event.preventDefault();
+
+    console.log('Dropped status:', status);
+    this.databaseService
+      .updateTask(this.daggedTaskId, {
+        status: status,
+      })
+      .subscribe((updatedTask) => {
+        console.log('Task updated:', updatedTask);
+      });
+    this.databaseService.loadTasks();
+  }
+
+  onDragOver(event: any, status: string) {
+    event.preventDefault();
+    console.log('onDragOver', status);
   }
 }
