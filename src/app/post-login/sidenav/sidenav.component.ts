eb700845ff72044f8d9b08e8b00034a2338fe4d5
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-10-01 13:29:54 +0200

    refactor: update Sidenav to support new routes and conditional display based on authentication
    - Refactored Sidenav component to align with new routing structure, removing obsolete `/home` paths.
    - Implemented `@if` condition to only display specific parts of the Sidenav (e.g., Summary, Add Task, Board, Contacts) for authenticated users.
    - Adjusted the Sidenav to hide the upper section for unauthenticated users when they access legal routes such as `/imprint` and `/privacy-policy`.
    - Improved Sidenav layout with `legal-bottom-aligned` class for unauthenticated users.

--- src/app/post-login/sidenav/sidenav.component.ts
+++ src/app/post-login/sidenav/sidenav.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
-  constructor(private router: Router) {}
+  isLoggedIn: boolean = true;
+  constructor(private router: Router, private authService: AuthService) {
+    if (
+      (this.router.url === '/imprint' ||
+        this.router.url === '/privacy-policy') &&
+      this.authService.currentUser === null
+    ) {
+      this.isLoggedIn = false;
+    }
+  }
 }
