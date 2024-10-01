c66b9fb635361db605e6c9533aa02f8a02399914
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-10-01 13:38:44 +0200

    refactor: auth guard removed comments

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,13 +3,12 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const token = localStorage.getItem('token'); // Check for the token
+  const token = localStorage.getItem('token');
   if (token) {
-    return true; // Token exists, allow access
+    return true;
   } else {
-    // Token does not exist, redirect to login
     const router = inject(Router);
-    router.navigate(['/']); // Change this to your login route
-    return false; // Deny access
+    router.navigate(['/']);
+    return false;
   }
 };
