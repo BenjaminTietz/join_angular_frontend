31e68915e9237a551d2595ba5124dc1c7c06eb9e
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-10-01 13:53:29 +0200

    feat: Start filling and styling Privacy Policy component
    - Added initial content to the Privacy Policy component in privacy-policy.component.html
    - Structured the content with appropriate headings and sections for clarity
    - Included key definitions related to privacy and data handling
    - Established a framework for users to understand data collection and usage policies
    - Set the last updated date to October 01, 2024

--- src/app/legal/privacy-policy/privacy-policy.component.ts
+++ src/app/legal/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../post-login/header/header.component';
+import { SidenavComponent } from '../../post-login/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SidenavComponent],
   templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+  styleUrl: './privacy-policy.component.scss',
 })
-export class PrivacyPolicyComponent {
-
-}
+export class PrivacyPolicyComponent {}
