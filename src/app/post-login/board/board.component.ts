6fa476d970dc5ff1ff4bfb1692d46bf3dfc96611
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-11-03 17:23:02 +0100

    refactor board & added feat added btns for mobile view to move single-tasks between status instead of drag & drop

--- src/app/post-login/board/board.component.ts
+++ src/app/post-login/board/board.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit, output } from '@angular/core';
-import { DatabaseService } from '../../services/database.service';
-import { Task } from '../../models/task.class';
-import { Observable } from 'rxjs';
-import { CommonModule } from '@angular/common';
-import { AddTaskComponent } from '../add-task/add-task.component';
-import { SubTask } from '../../models/subTask.class';
-import { HeaderComponent } from '../header/header.component';
-import { SidenavComponent } from '../sidenav/sidenav.component';
+import { Component, OnInit, output } from "@angular/core";
+import { DatabaseService } from "../../services/database.service";
+import { Task } from "../../models/task.class";
+import { Observable } from "rxjs";
+import { CommonModule } from "@angular/common";
+import { AddTaskComponent } from "../add-task/add-task.component";
+import { SubTask } from "../../models/subTask.class";
+import { HeaderComponent } from "../header/header.component";
+import { SidenavComponent } from "../sidenav/sidenav.component";
 
 @Component({
-  selector: 'app-board',
+  selector: "app-board",
   standalone: true,
   imports: [CommonModule, AddTaskComponent, HeaderComponent, SidenavComponent],
-  templateUrl: './board.component.html',
-  styleUrl: './board.component.scss',
+  templateUrl: "./board.component.html",
+  styleUrl: "./board.component.scss",
 })
 export class BoardComponent implements OnInit {
   tasks$!: Observable<Task[]>;
-  priorityIcons: { [key in 'urgent' | 'medium' | 'low']: string } = {
-    urgent: '/assets/img/icons/task_prio_urgent.png',
-    medium: '/assets/img/icons/task_prio_medium.png',
-    low: '/assets/img/icons/task_prio_low.png',
+  priorityIcons: { [key in "urgent" | "medium" | "low"]: string } = {
+    urgent: "/assets/img/icons/task_prio_urgent.png",
+    medium: "/assets/img/icons/task_prio_medium.png",
+    low: "/assets/img/icons/task_prio_low.png",
   };
   showAddTaskOverlay = false;
   showTaskDetailOverlay = false;
-  selectedStaus = '';
-  selectedCategory = '';
-  daggedTaskId = '';
+  selectedStaus = "";
+  selectedCategory = "";
+  daggedTaskId = "";
   draggedTaskIndex = 0;
   selectedTaskId: string | null = null;
   selectedTask: Task | null = null;
@@ -37,8 +37,8 @@ export class BoardComponent implements OnInit {
 
   getPriorityIcon(priority: string): string {
     return (
-      this.priorityIcons[priority as 'urgent' | 'medium' | 'low'] ||
-      '/assets/img/icons/task_prio_default.png'
+      this.priorityIcons[priority as "urgent" | "medium" | "low"] ||
+      "/assets/img/icons/task_prio_default.png"
     );
   }
 
@@ -54,16 +54,16 @@ export class BoardComponent implements OnInit {
   //drag and drop
 
   onDrag(event: DragEvent, index: number, taskId: string, status: string) {
-    console.log('onDrag', index);
-    console.log('taskId', taskId);
-    console.log('status', status);
+    console.log("onDrag", index);
+    console.log("taskId", taskId);
+    console.log("status", status);
     this.daggedTaskId = taskId;
     this.draggedTaskIndex = index;
   }
 
   onDrop(event: DragEvent, status: string) {
     event.preventDefault();
-    console.log('Dropped status:', status);
+    console.log("Dropped status:", status);
     const sourceArray = this.getSourceArrayByTaskId(this.daggedTaskId);
     if (sourceArray.length > 0) {
       this.databaseService
@@ -71,7 +71,7 @@ export class BoardComponent implements OnInit {
           status: status,
         })
         .subscribe((updatedTask) => {
-          console.log('Task updated:', updatedTask);
+          console.log("Task updated:", updatedTask);
           this.moveTaskBetweenArrays(
             this.draggedTaskIndex,
             sourceArray,
@@ -79,13 +79,26 @@ export class BoardComponent implements OnInit {
           );
         });
     } else {
-      console.error('Task not found in any array.');
+      console.error("Task not found in any array.");
     }
   }
 
   onDragOver(event: any, status: string) {
     event.preventDefault();
-    console.log('onDragOver', status);
+    console.log("onDragOver", status);
+  }
+
+  handleMoveToNewStatus(taskId: string, index: number, status: string) {
+    console.log("status", status);
+    const sourceArray = this.getSourceArrayByTaskId(taskId);
+    this.databaseService
+      .updateTask(taskId, {
+        status: status,
+      })
+      .subscribe((updatedTask) => {
+        console.log("Task updated:", updatedTask);
+        this.moveTaskBetweenArrays(index, sourceArray, status);
+      });
   }
   getSourceArrayByTaskId(taskId: string): Task[] {
     if (this.databaseService.todoTasks.some((task) => task.id === taskId)) {
@@ -107,24 +120,24 @@ export class BoardComponent implements OnInit {
   }
   moveTaskBetweenArrays(index: number, sourceArray: Task[], status: string) {
     let targetArray: Task[] = [];
-    if (status === 'todo') {
+    if (status === "todo") {
       targetArray = this.databaseService.todoTasks;
-    } else if (status === 'inProgress') {
+    } else if (status === "inProgress") {
       targetArray = this.databaseService.inProgressTasks;
-    } else if (status === 'awaitFeedback') {
+    } else if (status === "awaitFeedback") {
       targetArray = this.databaseService.awaitFeedbackTasks;
-    } else if (status === 'done') {
+    } else if (status === "done") {
       targetArray = this.databaseService.doneTasks;
     }
     const [task] = sourceArray.splice(index, 1);
-    console.log('old Task status:', task.status);
+    console.log("old Task status:", task.status);
     task.status = status;
-    console.log('new Task status:', task.status);
+    console.log("new Task status:", task.status);
     targetArray.push(task);
   }
 
   handleShowTaskDetailOverlay(task: Task) {
-    console.log('Task to be shown:', task);
+    console.log("Task to be shown:", task);
     this.selectedTask = task;
     this.selectedTaskId = task.id;
     this.showTaskDetailOverlay = true;
@@ -140,47 +153,47 @@ export class BoardComponent implements OnInit {
     this.selectedTask = task;
     this.databaseService.setTaskId(this.selectedTaskId);
     this.databaseService.setTaskData(this.selectedTask);
-    console.log('Task to edit:', task);
-    console.log('Task id:', this.selectedTaskId);
+    console.log("Task to edit:", task);
+    console.log("Task id:", this.selectedTaskId);
     this.databaseService.showEditTaskOverlay = true;
   }
 
   handleTaskSaved(updatedTask: Task) {
-    console.log('Task wurde gespeichert:', updatedTask);
+    console.log("Task wurde gespeichert:", updatedTask);
     this.showAddTaskOverlay = false;
     // todo : update task in the array
   }
 
   toggleCheckBox(task: Task, subTask: SubTask) {
-    console.log('Task to be updated:', task);
-    console.log('Subtask checked:', subTask.id);
+    console.log("Task to be updated:", task);
+    console.log("Subtask checked:", subTask.id);
     subTask.checked = !subTask.checked;
     this.databaseService
       .updateSubtaskStatus(task.id, subTask.id, subTask.checked)
       .subscribe({
         next: (response) => {
-          console.log('Subtask status updated', response);
+          console.log("Subtask status updated", response);
         },
         error: (error) => {
-          console.error('Error updating subtask status', error);
+          console.error("Error updating subtask status", error);
         },
         complete: () => {
-          console.log('Update complete');
+          console.log("Update complete");
         },
       });
   }
 
   handleDeleteTask(task: Task) {
-    console.log('Task to be deleted:', task);
+    console.log("Task to be deleted:", task);
     this.databaseService.deleteTask(task.id).subscribe({
       next: (response) => {
-        console.log('Task deleted:', response);
+        console.log("Task deleted:", response);
       },
       error: (error) => {
-        console.error('Error deleting task', error);
+        console.error("Error deleting task", error);
       },
       complete: () => {
-        console.log('Delete complete');
+        console.log("Delete complete");
       },
     });
   }
