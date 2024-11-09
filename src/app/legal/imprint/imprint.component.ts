b03341c240d75db3eeeb6a40d475a6f482bf0866
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-11-09 18:52:32 +0100

    recent work on pre-login components

--- src/app/legal/imprint/imprint.component.ts
+++ src/app/legal/imprint/imprint.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../post-login/header/header.component';
-import { SidenavComponent } from '../../post-login/sidenav/sidenav.component';
+import { Component } from "@angular/core";
+import { HeaderComponent } from "../../post-login/header/header.component";
+import { SidenavComponent } from "../../post-login/sidenav/sidenav.component";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-imprint',
+  selector: "app-imprint",
   standalone: true,
   imports: [HeaderComponent, SidenavComponent],
-  templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss',
+  templateUrl: "./imprint.component.html",
+  styleUrl: "./imprint.component.scss",
 })
-export class ImprintComponent {}
+export class ImprintComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  handleCloseImprint() {
+    if (this.authService.isLoggedIn) {
+      this.router.navigate(["/summary"]);
+    } else {
+      this.router.navigate(["/login"]);
+    }
+  }
+}
--- src/app/legal/privacy-policy/privacy-policy.component.ts
+++ src/app/legal/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../post-login/header/header.component';
-import { SidenavComponent } from '../../post-login/sidenav/sidenav.component';
+import { Component } from "@angular/core";
+import { HeaderComponent } from "../../post-login/header/header.component";
+import { SidenavComponent } from "../../post-login/sidenav/sidenav.component";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-privacy-policy',
+  selector: "app-privacy-policy",
   standalone: true,
   imports: [HeaderComponent, SidenavComponent],
-  templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss',
+  templateUrl: "./privacy-policy.component.html",
+  styleUrl: "./privacy-policy.component.scss",
 })
-export class PrivacyPolicyComponent {}
+export class PrivacyPolicyComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  handleClosePrivacyPolicy() {
+    if (this.authService.isLoggedIn) {
+      this.router.navigate(["/summary"]);
+    } else {
+      this.router.navigate(["/login"]);
+    }
+  }
+}
